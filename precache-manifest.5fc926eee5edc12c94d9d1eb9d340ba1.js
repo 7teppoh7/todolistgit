0b591f1930fb34bf0dc85aa9ffa3a1ff3ed377d8
7teppoh7 <you@example.com>
2020-04-28 22:34:01 +0400

    Updates

---
+++ precache-manifest.5fc926eee5edc12c94d9d1eb9d340ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40e2a400997fc25199de0db3f0b40af",
+    "url": "/todolistgit/index.html"
+  },
+  {
+    "revision": "0bbe6e9236cb3c78c297",
+    "url": "/todolistgit/static/css/main.6596147a.chunk.css"
+  },
+  {
+    "revision": "6b7c0d49fbce282511fd",
+    "url": "/todolistgit/static/js/2.07a6e364.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistgit/static/js/2.07a6e364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbe6e9236cb3c78c297",
+    "url": "/todolistgit/static/js/main.126293b4.chunk.js"
+  },
+  {
+    "revision": "66c5a245c8ea763e8344",
+    "url": "/todolistgit/static/js/runtime-main.a69be1a2.js"
+  }
+]);
